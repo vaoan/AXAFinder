10cff62eac8b95f2b25a20d93fdd87a418718446
GunB <heinerangarita@gmail.com>
2020-01-28 07:32:32 -0500

    Ant.d theme successfully configurated

--- config-overrides.js
+++ config-overrides.js
@@ -1,62 +1,34 @@
 // Overriding CreateReactApp settings, ref: https://github.com/arackaf/customize-cra
-const path = require('path')
-// const antdTheme = require('./theme.js')
+const themeConfig = require('./config-theme.js')
 const {
   override,
-  fixBabelImports,
-  // addLessLoader,
+  // fixBabelImports,
+  addLessLoader,
   useEslintRc,
   addDecoratorsLegacy,
   overrideDevServer,
   watchAll,
-  addExternalBabelPlugins,
+  addWebpackPlugin,
 } = require('customize-cra')
-
-const AntDesignThemePlugin = require('antd-theme-webpack-plugin')
-
-console.log(path.join(__dirname, './node_modules/antd'))
-
-const options = {
-  antDir: path.join(__dirname, './node_modules/antd'),
-  stylesDir: path.join(__dirname, './src/styles'),
-  varFile: path.join(__dirname, './src/styles/variables.less'),
-  mainLessFile: path.join(__dirname, './src/styles/index.less'),
-  themeVariables: [
-    '@primary-color',
-    '@secondary-color',
-    '@text-color',
-    '@text-color-secondary',
-    '@heading-color',
-    '@layout-body-background',
-    '@btn-primary-bg',
-    '@layout-header-background',
-    '@border-color-base',
-  ],
-  indexFileName: 'index.html',
-  generateOnce: false, // generate color.less on each compilation
-}
-
-const themePlugin = new AntDesignThemePlugin(options)
-// In plugins section, add this plugin instance
-// plugins: [ themePlugin, some other plugins ]
 
 module.exports = {
   webpack: override(
     // usual webpack plugin
     addDecoratorsLegacy(),
     useEslintRc(),
-
+    /*
     fixBabelImports('import', {
       libraryName: 'antd',
       libraryDirectory: 'es',
       style: true,
     }),
-    /*
+    */
+
     addLessLoader({
       javascriptEnabled: true,
-      modifyVars: antdTheme,
-    }),*/
-    addExternalBabelPlugins(themePlugin),
+      //modifyVars: antdTheme,
+    }),
+    addWebpackPlugin(themeConfig.plugin),
   ),
   devServer: overrideDevServer(
     // dev server plugin
